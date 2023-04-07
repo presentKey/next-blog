4a915134bdd7e9cd1faa7ac2255f246d463e1ba6
presentKey <kim.hyeonjae4@gmail.com>
2023-04-07 22:22:28 +0900

    new: 이전, 다음 포스트 카드 로직 구현

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,7 +1,6 @@
-import MarkdownViewer from '@/components/MarkdownViewer';
+import PostContent from '@/components/PostContent';
 import { getPostData } from '@/service/posts';
 import Image from 'next/image';
-import { AiTwotoneCalendar } from 'react-icons/ai';
 
 type Props = {
   params: {
@@ -10,7 +9,8 @@ type Props = {
 };
 
 export default async function PostPage({ params: { slug } }: Props) {
-  const { title, description, date, path, content } = await getPostData(slug);
+  const post = await getPostData(slug);
+  const { title, path, next, prev } = post;
 
   return (
     <article className='rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4'>
@@ -21,15 +21,10 @@ export default async function PostPage({ params: { slug } }: Props) {
         width={760}
         height={420}
       />
-      <section className='flex flex-col p-4'>
-        <div className='flex items-center self-end text-sky-600'>
-          <AiTwotoneCalendar />
-          <time className='font-semibold ml-2'>{date.toString()}</time>
-        </div>
-        <h1 className='text-4xl font-bold'>{title}</h1>
-        <p className='text-xl font-bold'>{description}</p>
-        <div className='w-44 border-2 border-sky-600 mt-4 mb-8'> </div>
-        <MarkdownViewer content={content} />
+      <PostContent post={post} />
+      <section>
+        {next && <p>{next.title}</p>}
+        {prev && <p>{prev.title}</p>}
       </section>
     </article>
   );
